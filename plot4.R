faabb4edfd5d4bd1afe97fb39a57da6e5c704526
Brooke Brickman <brooke.brickman@earthlink.net>
2015-05-05 19:32:13 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -4,7 +4,7 @@
 require(sqldf)
 require (tcltk)
 
-# open a subset of the data between the dates 
+# open a subset of the data for the 1st and the 2nd of February 2007:
 
 data<-read.csv.sql( file="household_power_consumption.txt", 
                     sep=";", sql="select * from file where Date = '1/2/2007' or Date = '2/2/2007'", 
@@ -22,18 +22,18 @@ png(file="plot4.png")
 
 par(mfrow=c(2,2))
 
-# create the plot of Global Active Power vs date/time:
+# create the plot of Global Active Power vs. date/time:
 
 plot(data$DateTime, data$Global_active_power, type="l", 
      bg="white", ann=FALSE)
 title(ylab="Global Active Power (kilowatts)")
 
-#create a plot of Voltage vs. date/time:
+#create the plot of Voltage vs. date/time:
 
 plot(data$DateTime, data$Voltage, type="l", 
      bg="white", ylab="Voltage", xlab="datetime")
 
-# create the plot of of variables Sub_metering 1, 2 and 3 vs. date/time:
+# create the plot of variables Sub_metering 1, 2 and 3 vs. date/time:
 
 plot(data$DateTime, data$Sub_metering_1, bg="white", type="n",
      ann=FALSE)
@@ -43,6 +43,7 @@ lines(data$DateTime, data$Sub_metering_3, col="blue")
 title(ylab="Energy sub metering")
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        lwd=2,col=c("black", "red", "blue"))
+
 # create the plot of Global Reactive Power vs date/time:
 
 plot(data$DateTime, data$Global_reactive_power,type="l", 
